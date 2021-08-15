6566857b6d23b6958386a41c15b6a05c4a991b62
Mindy Johnson <mindy.l.brock@gmail.com>
2021-08-15 13:44:02 -0500

    #bubble_sort works with one pass.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,4 +1,17 @@
 def bubble_sort(number_array)
+  index = 0
+
+  until index == number_array.length - 1 do
+    if number_array[index] > number_array[index + 1]
+      number_array.insert(index + 2, number_array[index])
+      number_array.delete_at(index)
+    end
+
+    index += 1
+  end
+
+  return number_array
+
 end
 
 p bubble_sort([4,3,78,2,0,2])
