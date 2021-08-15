b49d1480005118b5929bfed41dd936626b2437e5
Mindy Johnson <mindy.l.brock@gmail.com>
2021-08-15 14:14:37 -0500

    #bubble_sort completed and optimized

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,13 +1,25 @@
 def bubble_sort(number_array)
-  index = 0
+  no_swaps = false
+  req_passes = number_array.length - 1
 
-  until index == number_array.length - 1 do
-    if number_array[index] > number_array[index + 1]
-      number_array.insert(index + 2, number_array[index])
-      number_array.delete_at(index)
+  until no_swaps do
+    index = 0
+    no_swaps = true
+
+    while index < req_passes do
+      if number_array[index] > number_array[index + 1]
+        number_array.insert(index + 2, number_array[index])
+        number_array.delete_at(index)
+
+        no_swaps = false
+      end
+
+      index += 1
+
     end
 
-    index += 1
+    req_passes -= 1
+
   end
 
   return number_array
